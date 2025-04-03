1dd8b461f17cf1b3f12c8823536a4c58feb4341e
Jorge Villalobos <mail@minusfive.com>
2025-04-03 12:22:26 -0400

    feat(nvim): save file with CMD+s

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,22 +7,21 @@
 ---@param lhs string
 ---@param rhs string|function
 ---@param opts vim.keymap.set.Opts|?
--- local function map(mode, lhs, rhs, opts)
---   local keys = require("lazy.core.handler").handlers.keys
---   ---@cast keys LazyKeysHandler
---   -- do not create the keymap if a lazy keys handler exists
---   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
---     opts = opts or {}
---     opts.silent = opts.silent ~= false
---     if opts.remap and not vim.g.vscode then opts.remap = nil end
---     vim.keymap.set(mode, lhs, rhs, opts)
---   end
--- end
-
--- Move LazyVim's default "New File" keymap
--- vim.keymap.del("n", "<leader>fn")
--- map("n", "<leader>nf", "<cmd>enew<cr>", { desc = "New File" })
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then opts.remap = nil end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
 -- Delete problematic keymaps
 vim.keymap.del("n", "<leader>L")
 vim.keymap.del("n", "<leader>l")
+
+-- save file
+map({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
