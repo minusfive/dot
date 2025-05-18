5d114345609d2bcf882e195a073bc3ca9740967e
Jorge Villalobos <mail@minusfive.com>
2025-05-17 22:31:59 -0400

    feat(brew): add HOMEBREW_PROFILE to manage work vs. personal

--- scripts/brew.zsh
+++ scripts/brew.zsh
@@ -7,6 +7,26 @@ set -euo pipefail
 function {
     export HOMEBREW_BUNDLE_FILE_GLOBAL="$__dotfiles_scripts_dir/Brewfile"
 
+    function _v::pq() { print -P "  $1? ($(_v::color::fg red p)ersonal / $(_v::color::fg blue W)ork) $(_v::color::fg green %B⟩%b) " }
+
+    # Ask to choose which $HOMEBREW_PROFILE to use: "personal" or "work"
+    if [[ -z ${HOMEBREW_PROFILE:-} ]]; then
+        vared -p "$(_v::pq "Which $(_v::fmt::u HOMEBREW_PROFILE) would you like to use")" -c REPLY
+        if [[ $REPLY =~ ^[Pp]$ ]]; then
+            export HOMEBREW_PROFILE="personal"
+            _v::log::info "\$HOMEBREW_PROFILE set to \"$(_v::color::fg red personal)\""
+        elif [[ $REPLY == "" || $REPLY =~ ^[Ww]$ ]]; then
+            export HOMEBREW_PROFILE="work"
+            _v::log::info "\$HOMEBREW_PROFILE set to \"$(_v::color::fg blue work)\""
+        else
+            _v::log::error "Invalid input"
+            exit 1
+        fi
+        unset REPLY
+        echo "\n"
+    fi
+
+
     # Install Homebrew
     if [[ $(command -v brew) == "" ]]; then
         _v::log::info "$(_v::fmt::u Homebrew) not installed. Attempting install..."
