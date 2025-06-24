edd864031623a8e39fd6606fae1e09152431d5e0
Jorge Villalobos <mail@minusfive.com>
2025-06-23 23:08:21 -0400

    chore: spelling, icon tweaks

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -21,7 +21,7 @@ local lualine_vectorcode = {
 }
 
 local lualine_obsidian_vault = {
-  function() return "󱞁 " .. require("obsidian").get_client():vault_name() end,
+  function() return "󰎞  " .. require("obsidian").get_client():vault_name() end,
   cond = function()
     if package.loaded["obsidian"] == nil then
       return false
