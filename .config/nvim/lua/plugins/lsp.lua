fce711756df2d7c5455813df738b372e91b1d97c
Jorge Villalobos <mail@minusfive.com>
2025-07-05 22:49:35 -0400

    feat(nvim): lsp: limit diagnostic float to 90% of main window

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,6 +15,35 @@ local function toggleLSPClient(name)
   end
 end
 
+local win_diagnostics_opts = {
+  max_width = 0.9, -- 90% of the main window width
+  max_height = 0.9, -- 90% of the main window height
+}
+
+local function calc_max_dimensions()
+  -- Calculate max_width and max_height as a percentage of the main window's dimensions
+  local main_win = vim.api.nvim_get_current_win()
+  local width = vim.api.nvim_win_get_width(main_win)
+  local height = vim.api.nvim_win_get_height(main_win)
+
+  return {
+    max_width = math.floor(width * win_diagnostics_opts.max_width),
+    max_height = math.floor(height * win_diagnostics_opts.max_height),
+  }
+end
+
+---Store original `vim.lsp.util.open_floating_preview` function
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
+---Intercept `vim.lsp.util.open_floating_preview` to set default options
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  local max_dimensions = calc_max_dimensions()
+  opts = opts or {}
+  opts.max_width = opts.max_width or max_dimensions.max_width
+  opts.max_height = opts.max_height or max_dimensions.max_height
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
