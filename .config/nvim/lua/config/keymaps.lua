4179dcbd1190ceab8166e93a7f7ae02657dba12f
Jorge Villalobos <mail@minusfive.com>
2025-08-01 23:51:02 -0400

    feat(nvim): keymaps to navigate windows with arrow keys

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -25,3 +25,9 @@ vim.keymap.del("n", "<leader>l")
 
 -- Save all files with `󰘳  + s`
 map({ "i", "x", "n", "s" }, "<D-s>", "<cmd>wall<cr><esc>", { desc = "Save All Files" })
+
+-- Move to window using <ctrl>w + arrow keys. Which-key also remaps these to <leader>w + arrow keys
+map("n", "<C-w><Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-w><Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-w><Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-w><Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
