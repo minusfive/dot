5ece0e949bd30e4025671e32358604df21bd1820
Jorge Villalobos <mail@minusfive.com>
2025-09-27 12:08:12 -0400

    fix(nvim): restore JS LSP settings

--- .config/nvim/lua/plugins/lang/javascript.lua
+++ .config/nvim/lua/plugins/lang/javascript.lua
@@ -1,4 +1,40 @@
+local common_settings = {
+  -- preferGoToSourceDefinition = true,
+  preferences = {
+    -- importModuleSpecifier = "non-relative",
+    importModuleSpecifierEnding = "auto",
+    includePackageJsonAutoImports = "on",
+    -- preferGoToSourceDefinition = true,
+  },
+  -- updateImportsOnFileMove = { enabled = "always" },
+  -- suggest = {
+  --   completeFunctionCalls = true,
+  -- },
+  -- inlayHints = {
+  --   enumMemberValues = { enabled = true },
+  --   functionLikeReturnTypes = { enabled = true },
+  --   parameterNames = { enabled = "literals" },
+  --   parameterTypes = { enabled = true },
+  --   propertyDeclarationTypes = { enabled = true },
+  --   variableTypes = { enabled = false },
+  -- },
+}
+
 ---@type LazySpec
 return {
   { "dmmulroy/ts-error-translator.nvim" },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        vtsls = {
+          settings = {
+            typescript = common_settings,
+            javascript = common_settings,
+          },
+        },
+      },
+    },
+  },
 }
